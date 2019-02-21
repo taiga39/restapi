a29cd60c4c7c73b3976f181623e584d9bbab693a
taiga39 <ishikawa.tigers@gmail.com>
2019-02-21 18:53:01 +0900

    バリデーションの設定

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Http\Controllers\Controller;
+use Validator;
 
 class ProductsController extends Controller
 {
@@ -26,17 +27,27 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
-      $product = new Product;
-      $product->title = $request->title;
-      $product->body = $request->body;
-      $product->image = $request->image;
-      $product->price = $request->price;
-      $product->save();
-      return redirect('api/products');
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:10',
+            'body' => 'required|max:500',
+            'image' => 'required',
+            'price' => 'required',
+
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['文字数が多すぎます'],400);
+        }
+        $product = new Product;
+        $product->title = $request->title;
+        $product->body = $request->body;
+        $product->image = $request->image;
+        $product->price = $request->price;
+        $product->save();
+        return Product::all();
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified resource.ß
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -55,13 +66,22 @@ class ProductsController extends Controller
      */
     public function update(Request $request, $id)
     {
+      $validator = Validator::make($request->all(), [
+          'title' => 'required|max:100',
+          'body' => 'required|max:500',
+          'image' => 'required',
+          'price' => 'required',
+      ]);
+      if ($validator->fails()) {
+          return response()->json(['文字数が多すぎます'],400);
+      }
       $product = Product::find($id);
       $product->title = $request->title;
       $product->body = $request->body;
       $product->image = $request->image;
       $product->price = $request->price;
       $product->save();
-      return redirect("api/products/".$id);
+      return $product;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(['middleware' => ['api']], function(){
-  Route::resource('products', 'Api\ProductsController');
+  Route::resource('products', 'Api\ProductsController', [
+    'only' => ['index','show', 'store', 'update']]);
 });
